620bad504c021e4fbfdbbd376d3d00699d43913c
JohntPark <JohnTaeHeePark@gmail.com>
2018-10-23 15:42:48 -0700

    added controllers and routes for admin

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,4 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const { getCountryByProduct, getProductByCountry, addProductToCountry, deleteProductFromCountry } = require ('../controllers/admin')
+
+// getting the countries by the product
+router.get( '/:id', getCountryByProduct)
 
 
+// getting the products by country ID
+router.get( '/:id', getProductByCountry)
+
+// adding the product to the country
+router.post('/', addProductToCountry)
+
+// deleting the product from the country
+router.delete('/id', deleteProductFromCountry)
+
+module.exports = router
