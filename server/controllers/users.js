d42325e6beb9cce732a1605c60e2f8cdfeeda071
pngo12 <pngo12@gmail.com>
2018-10-24 09:25:34 -0700

    skeleton to auth user

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,5 +1,7 @@
 const bcrypt = require('bcryptjs');
 const User = require('../models/users')
+const jwt = require('jsonwebtoken')
+
 
 const createUser = async (req, res) => {
     try {
@@ -17,8 +19,26 @@ const createUser = async (req, res) => {
         })
     }
     catch (err) {
-        res.status(404).json({message:"Can't find route", error:err})
+        res.status(404).json({ message: "Can't find route", error: err })
     }
 }
 
-module.exports = {createUser}
+const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        let findUser = await User.findOne({ email })
+        if (!findUser) {
+            return res.status(404).send({ message: 'User not found' })
+        }
+        let authPassword = await bcrypt.compare(password, user.password)
+        if (authPassword) {
+    
+        }
+
+    }
+    catch(err){
+        res.status(404).send({message: 'Sorry something went wrong'})
+    }
+}
+
+module.exports = { createUser }
