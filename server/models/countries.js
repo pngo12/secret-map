c93f7abf220f1e0961801f73ce8a13d1e50c0c2d
pngo12 <pngo12@gmail.com>
2018-10-25 15:42:01 -0700

    cleaned up routes, renamed routes

--- server/models/countries.js
+++ server/models/countries.js
@@ -3,7 +3,6 @@ const Schema = mongoose.Schema;
 
 
 const CountriesSchema = new Schema({
-    // _id: Schema.Types.ObjectId,
     name: String,
     products: [{ type: Schema.Types.ObjectId, ref: 'products' }]
 });
