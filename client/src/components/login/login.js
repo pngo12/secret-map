83c05d72909201750053f51849f70638b1fc3832
Doogie Nguyen <dsnguye1@gmail.com>
2018-10-29 10:40:32 -0700

    login

--- client/src/components/login/login.js
+++ client/src/components/login/login.js
@@ -16,7 +16,7 @@ class Login extends Component {
   onLoginClick = e => {
     e.preventDefault();
     let { username, password } = this.state
-    this.props.login({username, password})
+    this.props.login({ username, password })
     this.setState({ loginClicked: true })
   }
 
@@ -25,55 +25,75 @@ class Login extends Component {
 
   render() {
     return (
-      <section className="hero is-fullheight has-background-grey">
-        {
-          this.state.submitClicked
-            ? <Redirect to='/' />
-            :
-            <div className="section is-medium">
-              <div className="container notification loggingOn" style={{ height: 500, paddingRight: 24 }}>
-                <div id="login-page">
-                  <form className="formGrouping">
-                    <img src={Glidewell} style={{ width: '41%', height: '30%', marginBottom: '2%' }} alt="logo" />
-                    <div className="form-group">
-                      <label style={{ fontSize: '1em' }}> Username </label>
-                      <br />
-                      <input
-                        className="input is-normal"
-                        style={{ width: '25%' }}
-                        value={this.state.username}
-                        type="text"
-                        name="username"
-                        placeholder="Username"
-                        onChange={this.handleOnChange}
-                      />
-                    </div>
-                    <div className="form-group">
-                      <label style={{ fontSize: '1em' }}> Password </label>
-                      <br />
-                      <input
-                        className="input is-normal"
-                        style={{ width: '25%' }}
-                        value={this.state.password}
-                        type="password"
-                        name="password"
-                        placeholder="Password"
-                        onChange={this.handleOnChange}
-                      />
-                    </div>
-                    <button
-                      type="submit"
-                      className='button is-link'
-                      style={{ marginTop: '2%' }}
-                      onClick={this.onLoginClick}>
-                      Submit
-                  </button>
-                  </form>
-                </div>
+      
+      <div>
+      { this.state.submitClicked ? <Redirect to='/' />
+      :
+      <div id="login-page">
+            <img className = "border" src={Glidewell} style={{ margin: 15 }} alt="logo" />
+            <div className="form">
+              <h3 style={{ margin: 7 }}>Username</h3>
+              <input class="input" style={{ margin: 7 }} type="text" value={this.state.username} name="username" onChange={this.handleOnChange} placeholder="username" />
+              <h3 style={{ margin: 7 }}>Password</h3>
+              <input id="test" class="input" style={{ margin: 7 }} type="password" value={this.state.password} name="password" onChange={this.handleOnChange} placeholder="password" />
+              <div class="control">
+                <a class="button is-danger" style={{ margin: 7 }} onClick={this.onLoginClick}>Login</a>
               </div>
             </div>
-        }
-      </section>
+          
+      </div>
+      }
+      </div>
+
+      // <section className="hero is-fullheight has-background-grey">
+      //   {
+      //     this.state.submitClicked
+      //       ? <Redirect to='/' />
+      //       :
+      //       <div className="section is-medium">
+      //         <div className="container notification loggingOn" style={{ height: 500, paddingRight: 24 }}>
+      //           <div id="login-page">
+      //             <form className="formGrouping">
+      //               <img src={Glidewell} style={{ width: '41%', height: '30%', marginBottom: '2%' }} alt="logo" />
+      //               <div className="form-group">
+      //                 <label style={{ fontSize: '1em' }}> Username </label>
+      //                 <br />
+      //                 <input
+      //                   className="input is-normal"
+      //                   style={{ width: '25%' }}
+      //                   value={this.state.username}
+      //                   type="text"
+      //                   name="username"
+      //                   placeholder="Username"
+      //                   onChange={this.handleOnChange}
+      //                 />
+      //               </div>
+      //               <div className="form-group">
+      //                 <label style={{ fontSize: '1em' }}> Password </label>
+      //                 <br />
+      //                 <input
+      //                   className="input is-normal"
+      //                   style={{ width: '25%' }}
+      //                   value={this.state.password}
+      //                   type="password"
+      //                   name="password"
+      //                   placeholder="Password"
+      //                   onChange={this.handleOnChange}
+      //                 />
+      //               </div>
+      //               <button
+      //                 type="submit"
+      //                 className='button is-link'
+      //                 style={{ marginTop: '2%' }}
+      //                 onClick={this.onLoginClick}>
+      //                 Submit
+      //             </button>
+      //             </form>
+      //           </div>
+      //         </div>
+      //       </div>
+      //   }
+      // </section>
     );
   }
 }
@@ -86,6 +106,7 @@ class Login extends Component {
  * - Make sure what you are getting back frmo the axios call 
  * - Make sure reducer is updating state properly by using the redux dev tools in your browser
  */
+
 const mapPropsToDispatch = dispatch => ({
   login: user => dispatch(login(user))
 });
