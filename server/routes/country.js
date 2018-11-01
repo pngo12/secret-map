dd905fd11c44c4752bf93747e7aa7a950b0c9169
pngo12 <pngo12@gmail.com>
2018-11-01 14:26:42 -0700

    merging changes

--- server/routes/country.js
+++ server/routes/country.js
@@ -3,8 +3,6 @@ const router = express.Router();
 const { 
     getCountryAll,
     findOneCountry,
-    // addProductToCountry, 
-    // removeProductFromCountry
 } = require ('../controllers/country')
 
 // Query for all countries in DB
@@ -12,11 +10,5 @@ router.get('/', getCountryAll)
 
 // Query for ONE country in DB with it's products
 router.get('/:name', findOneCountry)
-
- // adding a product to a country
-// router.post('/new', addProductToCountry)
-
- // deleting the product from the country
-// router.put('/', removeProductFromCountry)
 
 module.exports = router
